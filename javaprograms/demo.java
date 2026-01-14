682e0a133df086629cda80308dd07ecea48044a7
MRADUVENDRA <mraduvendragillu1234@gmail.com>
2026-01-14 19:17:48 +0530

    Assisted Problems

--- javaprograms/demo.java
+++ javaprograms/demo.java
@@ -1,5 +1,5 @@
-class Bridgelabz {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
+class demo{
+	public static void main(String args[]){
+		System.out.println("First Java file");
+	}
 }
