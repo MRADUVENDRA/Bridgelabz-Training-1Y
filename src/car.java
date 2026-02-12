aa6bc92c77a78711bbdad1755d05186bc9c6b143
MRADUVENDRA <mraduvendragillu1234@gmail.com>
2026-02-12 18:34:26 +0530

    hii

---
+++ src/car.java
@@ -0,0 +1,9 @@
+public class car {
+    public static void main(String[] args) {
+        String name;
+        String Company;
+        double price;
+        String model;
+        System.out.println("Pagani");
+    }
+}
